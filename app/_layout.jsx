fea7d727bec295e4d0c8a3f33cd2d1c7da8e7f4b
CH2S3N <sanchomiguelbalmes@gmail.com>
2024-10-14 17:25:45 +0800

    reinstalled missing dependencies

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,11 +2,11 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  useFonts({
-    'montserrat':require('./../assets/fonts/Montserrat-Regular.ttf'),
-    'montserrat-medium':require('./../assets/fonts/Montserrat-Medium.ttf'),
-    'montserrat-bold':require('./../assets/fonts/Montserrat-Bold.ttf'),
-  })
+  // useFonts({
+  //   'montserrat':require('./../assets/fonts/Montserrat-Regular.ttf'),
+  //   'montserrat-medium':require('./../assets/fonts/Montserrat-Medium.ttf'),
+  //   'montserrat-bold':require('./../assets/fonts/Montserrat-Bold.ttf'),
+  // })
   return (
     <Stack>
       <Stack.Screen name="(tabs)" 
