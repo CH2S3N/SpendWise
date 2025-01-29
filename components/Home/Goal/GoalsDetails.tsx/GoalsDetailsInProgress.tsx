d24c777f66c3a4b0b54e88e92da6bac02aeb13de
Sancho Balmes <sanchomiguelbalmes@gmail.com>
2025-01-29 21:29:14 +0800

    .

--- components/Home/Goal/GoalsDetails.tsx/GoalsDetailsInProgress.tsx
+++ components/Home/Goal/GoalsDetails.tsx/GoalsDetailsInProgress.tsx
@@ -14,6 +14,10 @@ export default function InProgress({ goal }: Props) {
     const progress = accumulatedAmount / goal.amount;
     const progressPercentage = (progress * 100).toFixed(2);
 
+
+    if (accumulatedAmount == goal.amount) {
+        return null;
+    }
     return (
         <Card
             content={
